8af8574ee9a3123577093f354508df96d47017ac
Kishor Kabadi <kishor.kabadi@intelliquity.in>
2023-07-19 12:43:45 +0530

    Lazy Loading

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LazyLadingApp'`, () => {
+  it(`should have as title 'ModelLoding'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LazyLadingApp');
+    expect(app.title).toEqual('ModelLoding');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LazyLadingApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ModelLoding app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//commented to implement lazy loading
+// import {CompanyModule}  from './company/company.module';
+// import { PersonModule } from './person/person.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // CompanyModule,//Module Binding commented to implement lazy loading
+    // PersonModule//Module Binding commented to implement lazy loading
   ],
   providers: [],
   bootstrap: [AppComponent]
